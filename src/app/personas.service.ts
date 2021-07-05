2478997ae17441627164e16584a0cdf93bfedd0a
Ismael Dominguez <ismaeldom11@gmail.com>
2021-07-05 18:57:54 -0500

    Se realiza inyección de un servicio a otro
    Al servicio PersonasService se le inyecta el servicio LoggingService.

--- src/app/personas.service.ts
+++ src/app/personas.service.ts
@@ -1,5 +1,8 @@
+import { Injectable } from '@angular/core';
+import { LoggingService } from './LoggingService.service';
 import { Persona } from './personas/persona.model';
 
+@Injectable()//Se inyecta un servicio
 export class PersonasService {
   personas: Persona[] = [
     new Persona('Juan', 'Perez'),
@@ -7,7 +10,10 @@ export class PersonasService {
     new Persona('Karla', 'Lara'),
   ];
 
+  constructor(private loggingService: LoggingService) {}
+
   personaAgregada(persona: Persona) {
+    this.loggingService.enviaMensajeAConsola("Se agrega persona desde el servicio: " + persona.nombre);
     this.personas.push(persona);
   }
 }
