ed2a7fb2cc88addb2de54535690553536c1261f2
Ismael Dominguez <ismaeldom11@gmail.com>
2021-07-05 16:55:32 -0500

    Se cambia Event Binding por Referencia Local
    Se comenta el código usado para Event Binding y se agrega el código para usar Referencia Local.

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -10,11 +10,12 @@ export class FormularioComponent {
   //Evento para propagar al componente padre
   @Output() personaCreada = new EventEmitter<Persona>();
 
-  nombreInput:string = '';
-  apellidoInput:string = '';
+  //nombreInput:string = ''; //Usando Event Binding
+  //apellidoInput:string = ''; //Usando Event Binding
   
-  addPersona(){
-    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+  addPersona(nombreInput:HTMLInputElement, apellidoInput:HTMLInputElement){
+    let persona1 = new Persona(nombreInput.value, apellidoInput.value); //Usando Referencia Local
+    //let persona1 = new Persona(this.nombreInput, this.apellidoInput); //Usando Event Binding
     //this.personas.push(persona1);
     this.personaCreada.emit(persona1);
   }
